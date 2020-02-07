623d99c4cb54b8a430c02940d0ab52eb4588cb1a
Adrian <59113139+adrian-gvr@users.noreply.github.com>
2020-02-07 22:16:26 +0100

    Update chessboard.js

--- chessboard.js
+++ chessboard.js
@@ -3,3 +3,26 @@
 * At each position of the grid there is either a space or a "#" character. 
 * The characters should form a chessboard.
 */
+let x =" ";
+for (colonna=1; colonna<9; colonna++)
+{
+  for (riga=1; riga<9; riga++)
+  {
+    if(colonna%2===0)
+    {
+      if(riga%2===0)
+        x=x+" ";
+      else
+        x=x+"#";
+    }
+    else
+    {
+      if(riga %2===0)
+        x=x+"#";
+      else
+        x=x+" ";
+    }
+  }
+  console.log(x);
+  x=" ";
+}
